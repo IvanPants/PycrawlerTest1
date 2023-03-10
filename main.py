0d90ff0bf83704bcc8ee4021f4fe699f71d7b2f2
Terminator2006 <vania.pants@yandex.ru>
2023-03-10 11:17:42 +0300

    new request

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import html.parser
 from fake_useragent import UserAgent
-
+import webbrowser as wb
 
 def create_header():
     ua = UserAgent()
@@ -14,8 +14,12 @@ def yandex_search():
     url = 'https://yandex.ru/search/?text='
     return url
 
+def google_search():
+    url = "https://search.sapti.me/search?q="
+    return url
 
-def get_yandex_search():
+
+def get_yandex_search():    # ? reworked function
     user_input = input('Введите запрос : ')
     url = yandex_search() + user_input
     header = create_header()
@@ -24,16 +28,24 @@ def get_yandex_search():
     return response
 
 
-def google_search():
-    pass
+def get_google_search():    # ? reworked function
+    user_input = input('Введите запрос : ')
+    url = google_search() + user_input
+    # wb.open(url)
+    header = create_header()
+    response = requests.get(url=url, headers=header)
+    response.encoding = 'utf-8'
+    return response
 
 
 def main():
-    response = get_yandex_search()
+    response = get_google_search()
     soup = BeautifulSoup(response.content, 'html.parser')
-    with open('yandex_search_out.html', 'w', encoding='utf-8') as search_file:
+    searching_result = set([x['href'] for x in soup.find('div', id='urls').find_all('a')])
+    with open('google_search_out.html', 'w', encoding='utf-8') as search_file:
         search_file.write(str(soup.prettify()))
-    
+    print(searching_result)
+
 
 if __name__ == '__main__':
     main()
